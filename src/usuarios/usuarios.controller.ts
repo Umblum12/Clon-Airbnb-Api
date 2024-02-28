5ea2f10502338b920aa8f57591a8b1e7b99aeabc
Umblum12 <abelardo_cruzleos@hotmail.com>
2024-02-28 08:18:18 -0700

    UpdateSendEmail

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -2,10 +2,53 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import* as nodemailer from 'nodemailer';
 
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
+
+
+
+    @Post(':mail')
+    async sendEmail(@Param('mail') mail: string) {
+      try {
+        // Create a Nodemailer transporter
+        const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: 'noreplyclassstop@gmail.com',
+            pass: 'buqv rssv vyim uvjd'
+          }
+        });
+  
+        // Setup email data
+        const mailOptions = {
+          from: 'noreplyclassstop@gmail.com',
+          to: mail,
+          subject: 'Verificación de Correo - ClassTop',
+          html: `
+            <p>Hola,</p>
+            <p>Gracias por registrarte en ClassTop. Por favor, haz clic en el siguiente botón para verificar tu correo electrónico:</p>
+            <a href="URL_DE_VERIFICACION" style="display: inline-block; padding: 10px 20px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 5px;">Verificar Correo</a>
+            <p>Si no has creado una cuenta en ClassTop, puedes ignorar este mensaje.</p>
+          `
+        };
+  
+        // Send the email
+        const info = await transporter.sendMail(mailOptions);
+  
+        console.log('Email sent: ' + info.response);
+  
+        // Respond with success
+        return { message: 'Email sent successfully' };
+      } catch (error) {
+        console.error('Error sending email:', error);
+        throw new Error('An error occurred while sending the email');
+      }
+    }
+  
+
 
   @Post()
   create(@Body() createUsuarioDto: CreateUsuarioDto) {
